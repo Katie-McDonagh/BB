07a9a5b28546d3897bb586b019b6932f1e339690
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-28 17:05:44 +0000

    bikes mocked into the docking station test files

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,13 +2,15 @@ require 'docking_station'
 
 describe DockingStation do 
 
+  let(:bike) { double :bike }
+
   it 'has a default capacity' do
     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
 
   describe '#release_bike' do
   it 'releases a bike' do
-    bike = Bike.new
+    bike = double(:bike, broken?: false)
     subject.dock(bike)
     expect(subject.release_bike).to eq(bike)
   end
@@ -18,25 +20,22 @@ describe DockingStation do
   end
 
   it 'raises an error if the user tries to release a broken bike' do 
-    bike = Bike.new
-    bike.report_broken
+    bike = double(:bike, broken?: true)
     subject.dock(bike)
     expect { subject.release_bike }.to raise_error 'No bikes available'
   end
 end
   describe "#dock" do 
     it "docks a bike" do
-      bike = Bike.new
+      bike = (:bike)
       expect(subject.dock(bike)).to eq(bike)
     end
 
     it "returns an error if the docking station is full" do
-      subject.capacity.times { subject.dock Bike.new }
-      expect{ subject.dock(Bike.new) }.to raise_error 'Docking Station full'
+      subject.capacity.times { subject.dock(:bike) }
+      expect{ subject.dock((:bike)) }.to raise_error 'Docking Station full'
     end
 
   end
-
-
 
 end

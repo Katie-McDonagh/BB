f14b7a62b09f9d97a80f8dcbc40e1f205e097b7b
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-28 13:16:29 +0000

    docking station allows a bike to be docked

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,18 @@
+require_relative 'bike'
 class DockingStation 
+
+  attr_reader :bike
+
+  def initialize
+    @bike = Bike.new
+  end
+
  def release_bike
-  Bike.new
+  @bike
  end
+
+ def dock(bike) 
+  bike = @bike
+ end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,14 @@ describe DockingStation do
 
     it { is_expected.to respond_to(:release_bike) }
 
-    it "releases a new bike when the method release bike is called" do
-      bike = DockingStation.new.release_bike
-      expect(bike.working?).to eq(true)
+    it "releases working bikes" do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
+    it "docks a bike" do
+      bike = subject.release_bike
+      expect(subject.dock(bike)).to eq(bike)
     end
 
 end

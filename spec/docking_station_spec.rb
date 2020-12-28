960d88fb00fdeca6006a72b905bccf9fd5bd7d28
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-28 12:54:24 +0000

    release bike method releases a working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,12 @@
 require 'docking_station'
 
 describe DockingStation do 
-  
+
     it { is_expected.to respond_to(:release_bike) }
+
+    it "releases a new bike when the method release bike is called" do
+      bike = DockingStation.new.release_bike
+      expect(bike.working?).to eq(true)
+    end
 
 end

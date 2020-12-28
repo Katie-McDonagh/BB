49057386ea4f8c6eafe91abe24afb7b7a3fced7c
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-28 14:35:49 +0000

    docks and releases the same bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,13 @@ require 'docking_station'
 
 describe DockingStation do 
 
-    it { is_expected.to respond_to(:release_bike) }
-
-    it "releases working bikes" do
-      bike = subject.release_bike
-      expect(bike).to be_working
-    end
+  describe '#release_bike' do
+  it 'releases a bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq(bike)
+  end
+end
 
     it "docks a bike" do
       bike = Bike.new
@@ -19,5 +20,7 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.bike).to eq(bike)
     end
+
+
 
 end

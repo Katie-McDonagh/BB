6663ec0c57638e7d6b54923ac41b4844e5c2df92
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-29 17:27:06 +0000

    van class can pick up a broken bike

--- lib/vans.rb
+++ lib/vans.rb
@@ -1,3 +1,13 @@
 class Van 
 
+  attr_reader :broken_bikes
+
+  def initialize
+    @broken_bikes = []
+  end
+
+  def pick_up_bike(bike)
+    @broken_bikes.push(bike)
+  end
+
 end
--- spec/vans_spec.rb
+++ spec/vans_spec.rb
@@ -2,5 +2,10 @@ require 'vans'
 
 describe Van do
 
+  let(:bike) { double :bike }
 
+  it "can store broken bikes" do
+    subject.pick_up_bike(:bike)
+    expect(subject.broken_bikes).to include(:bike)
+  end
 end

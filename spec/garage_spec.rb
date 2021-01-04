892d77c42ed5a37fa0b62ea5c4f019c87e5d360e
Tim <timcpb@hotmail.com>
2021-01-04 21:12:05 +0000

    add van double to garage 'receives broken bikes from the van'

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -3,11 +3,12 @@ require 'garage'
 describe Garage do
   
   it "receives broken bikes from the van" do
-    van = Van.new
+    # van = Van.new
     bike = double(:bike, broken: true)
     bikes = [bike]
-    # allow(van).to receive(:pick_up_bike).with(bikes)
-    # allow(van).to receive(:drop_off_bikes).with(subject)
+    van = double(:van)
+    allow(van).to receive(:pick_up_bike).with(bikes)
+    allow(van).to receive(:drop_off_bikes).with(subject) { subject.bikes_to_fix.push(bike) }
     van.pick_up_bike(bikes)
     van.drop_off_bikes(subject)
     expect(subject.bikes_to_fix).to include(bike)

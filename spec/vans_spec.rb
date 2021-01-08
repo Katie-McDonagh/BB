21a069bca0ee42fccb4c6fcc93b9bc73c78728ae
Tim <timcpb@hotmail.com>
2021-01-08 18:36:26 +0000

    refactor

--- spec/vans_spec.rb
+++ spec/vans_spec.rb
@@ -7,7 +7,6 @@ describe Van do
 
   it "is passed broken bikes by the docking station" do
     bike = double(:bike, broken: true)
-    # docking_station = double(:docking_station, bikes: [bike])
     bikes = [bike]
     subject.pick_up_bike(bikes)
     expect(subject.broken_bikes).to include(bike)

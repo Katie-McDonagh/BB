82ab313feac00c65a824a60108f9b58eb87207c9
Tim <timcpb@hotmail.com>
2021-01-08 18:29:38 +0000

    refactor

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,6 +2,13 @@ require 'garage'
 
 describe Garage do
 
+  it "fixes bikes" do
+    bike = double(:bike, broken?: true)
+    subject.bikes_to_fix.push(bike)
+    expect(bike).to receive(:fix_bike)
+    subject.fix_bikes
+  end
+
   it "moves bikes to the working_bikes array" do
     bike = double(:bike)
     allow(bike).to receive(:fix_bike)
@@ -10,10 +17,4 @@ describe Garage do
     expect(subject.working_bikes).to include(bike)
   end
 
-  it "fixes bikes" do
-    bike = double(:bike, broken?: true)
-    subject.bikes_to_fix.push(bike)
-    expect(bike).to receive(:fix_bike)
-    subject.fix_bikes
-  end
 end
